9432d4f43cec2bda402f8d3917ffaca2f69dccde
John Murillo Morales <john/yon@hotmail.com>
2023-11-27 20:08:28 -0600

    modified:   src/App.js     new file:   src/azure-image-analysis.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
 import './App.css';
+import { analyzeImage } from './api'; // Importar la función analyzeImage
+
 
 function App() {
   const [imageUrl, setImageUrl] = useState('');
+  const [processing, setProcessing] = useState(false); // Indicador de procesamiento
+  const [results, setResults] = useState(null); // Resultados del análisis
 
-  const handleAnalyzeImage = () => {
-    // Aquí va el código para analizar la imagen
+  const handleAnalyzeImage = async () => {
+    setProcessing(true); // Mostrar indicador de procesamiento
+
+    try {
+      const analysisResults = await analyzeImage(imageUrl); // Llamar a la función analyzeImage
+      setResults(analysisResults); // Actualizar los resultados del análisis
+    } catch (error) {
+      console.error(error);
+    }
+
+    setProcessing(false); // Ocultar indicador de procesamiento
   };
 
   const handleGenerateImage = () => {
     // Aquí va el código para generar la imagen
+  };
+
+  const DisplayResults = () => {
+    if (results) {
+      // Mostrar los resultados de la llamada API en un formato legible
+      return (
+        <div>
+          <h2>Resultados del análisis:</h2>
+          <pre>{JSON.stringify(results, null, 2)}</pre>
+        </div>
+      );
+    } else {
+      return null;
+    }
   };
 
   return (
@@ -23,6 +50,8 @@ function App() {
       />
       <button className="button" onClick={handleAnalyzeImage}>Analizar imagen</button>
       <button className="button" onClick={handleGenerateImage}>Generar imagen</button>
+      {processing && <p>Procesando...</p>} // Mostrar indicador de procesamiento si está en progreso
+      <DisplayResults /> // Mostrar los resultados del análisis
     </div>
   );
 }
---
+++ src/azure-image-analysis.js
@@ -0,0 +1,29 @@
+
+// Importar los módulos necesarios
+const axios = require('axios');
+
+// Crear la función analyzeImage
+async function analyzeImage(imageUrl) {
+  try {
+    // Hacer una solicitud a la API de Image Analysis 4.0 de Azure AI
+    const response = await axios.post('https://api.cognitive.microsoft.com/vision/v4.0/analyze', {
+      url: imageUrl,
+      visualFeatures: ['Categories', 'Description', 'Tags']
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        
+        'Ocp-Apim-Subscription-Key': '1ad1adf6b1da40d3a5c964e1b1ff650d'
+      }
+    });
+
+    // Devolver la respuesta JSON de la API
+    return response.data;
+  } catch (error) {
+    console.error('Error analyzing image:', error);
+    throw error;
+  }
+}
+
+// Exportar la función analyzeImage
+module.exports = analyzeImage;
