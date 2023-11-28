9588075bfadd94646460dbc33217bfa287ec9832
John Murillo Morales <john/yon@hotmail.com>
2023-11-27 18:20:11 -0600

    new file:   src/App.css modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+  const [imageUrl, setImageUrl] = useState('');
 
+  return (
+    <div>
+
+      <h1 className="title">Título</h1>
+      <button className="button">Analizar imagen</button>
+
+      <h1>Título</h1>
+      <input 
+        type="text" 
+        value={imageUrl} 
+        onChange={e => setImageUrl(e.target.value)} 
+        placeholder="Escribe la URL de la imagen aquí"
+      />
+      <button style={{backgroundColor: 'blue', color: 'white'}}>Analizar imagen</button>
+      <button onClick={() => console.log('Análisis de imagen')}>Analizar imagen</button>
+      <button onClick={() => console.log('Generación de imagen')}>Generar imagen</button>
+    </div>
+  );
+}
 export default App;
